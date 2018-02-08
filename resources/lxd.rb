59ade5deb47c5a544dd2dad70444952168c63f01
Sean Zachariasen <thewyzard@hotmail.com>
2018-02-08 16:30:26 -0700

    maybe

--- resources/lxd.rb
+++ resources/lxd.rb
@@ -179,7 +179,7 @@ action_class do
     apt_update 'update' if node['platform_family'] == 'debian'
 
     if should_snap?
-      package %w(fuse squashfuse) do
+      apt_package %w(fuse squashfuse) do
         ignore_failure true if node['lsb']['codename'] == 'trusty' # squashfuse is more of a nested host need.  Don't use trusty for nesting.
       end
       package 'snapd' # watchout: there could be PATH issues here, after this, if snap was not previously installed...

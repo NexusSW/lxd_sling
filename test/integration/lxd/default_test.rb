df6a95f8ad1318b226c0880b27ce4d5df527af8d
Sean Zachariasen <thewyzard@hotmail.com>
2018-02-20 17:43:40 -0700

    more tests

--- test/integration/lxd/default_test.rb
+++ test/integration/lxd/default_test.rb
@@ -3,6 +3,18 @@
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
 
+require 'yaml'
+
 describe port(8443) do
   it { should be_listening }
+end
+
+describe bridge('lxdbr0') do
+  it { should exist }
+end
+
+describe 'default profile' do
+  it 'has eth0 attached to lxdbr0' do
+    expect(YAML.load(command('lxc profile show default').stdout)['devices']['eth0']['parent']).to eq 'lxdbr0'
+  end
 end
